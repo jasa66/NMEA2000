02c6f4e35f5bb6b14ced7fd75aa150659e46ef8b
Jaakko Saarela <jaakko.saarela@turkuamk.fi>
2025-04-03 17:13:22 +0300

    FIX: 130578 handlers

--- src/N2kMessages.cpp
+++ src/N2kMessages.cpp
@@ -2197,46 +2197,50 @@ bool ParseN2kPGN130577(const tN2kMsg &N2kMsg,
   return true;
 }
 
-bool ParseN2kPGN130578(const tN2kMsg &N2kMsg,
-      double &LongitudalSpeedWaterRef,
-      double &TraverseSpeedWaterRef,
-      double &LongitudalSpeedGroundRef,
-      double &TraverseSpeedGroundRef,
-      double &SternSpeedWaterRef,
-      double &SternSpeedGroundRef)
- {
-  if (N2kMsg.PGN!=130578L) return false;
-  int Index=0;
-  LongitudalSpeedWaterRef = N2kMsg.Get2ByteUDouble(0.0001,Index);
-  TraverseSpeedWaterRef = N2kMsg.Get2ByteUDouble(0.0001,Index);
-  LongitudalSpeedGroundRef = N2kMsg.Get2ByteUDouble(0.0001,Index);
-  TraverseSpeedGroundRef = N2kMsg.Get2ByteUDouble(0.0001,Index);
-  SternSpeedWaterRef = N2kMsg.Get2ByteUDouble(0.0001,Index);
-  SternSpeedGroundRef = N2kMsg.Get2ByteUDouble(0.0001,Index);
-  return true;
- }
+/********************************************************************************************/
+//*****************************************************************************
 
- void SetN2kPGN130577(tN2kMsg &N2kMsg, 
-      double LongitudalSpeedWaterRef,
-      double TraverseSpeedWaterRef,
-      double LongitudalSpeedGroundRef, 
-      double TraverseSpeedGroundRef, 
-      double SternSpeedWaterRef, 
-      double SternSpeedGroundRef)
+
+
+bool ParseN2kPGN130578(const tN2kMsg &N2kMsg,
+  double &LongitudalSpeedWaterRef,
+  double &TraverseSpeedWaterRef,
+  double &LongitudalSpeedGroundRef,
+  double &TraverseSpeedGroundRef,
+  double &SternSpeedWaterRef,
+  double &SternSpeedGroundRef)
 {
-  N2kMsg.SetPGN(130578L);
-  N2kMsg.Priority=3;
-  N2kMsg.Add2ByteUDouble(LongitudalSpeedWaterRef,0.0001);
-  N2kMsg.Add2ByteUDouble(TraverseSpeedWaterRef,0.0001);
-  N2kMsg.Add2ByteUDouble(LongitudalSpeedGroundRef,0.0001);
-  N2kMsg.Add2ByteUDouble(TraverseSpeedGroundRef,0.0001);
-  N2kMsg.Add2ByteUDouble(SternSpeedWaterRef,0.0001);
-  N2kMsg.Add2ByteUDouble(SternSpeedGroundRef,0.0001);
+if (N2kMsg.PGN!=130578L) return false;
+int Index=0;
+LongitudalSpeedWaterRef = N2kMsg.Get2ByteDouble(0.001,Index);
+TraverseSpeedWaterRef = N2kMsg.Get2ByteDouble(0.001,Index);
+LongitudalSpeedGroundRef = N2kMsg.Get2ByteDouble(0.001,Index);
+TraverseSpeedGroundRef = N2kMsg.Get2ByteDouble(0.001,Index);
+SternSpeedWaterRef = N2kMsg.Get2ByteDouble(0.001,Index);
+SternSpeedGroundRef = N2kMsg.Get2ByteDouble(0.001,Index);
+return true;
+}
+
+void SetN2kPGN130577(tN2kMsg &N2kMsg, 
+  double LongitudalSpeedWaterRef,
+  double TraverseSpeedWaterRef,
+  double LongitudalSpeedGroundRef, 
+  double TraverseSpeedGroundRef, 
+  double SternSpeedWaterRef, 
+  double SternSpeedGroundRef)
+{
+N2kMsg.SetPGN(130578L);
+N2kMsg.Priority=3;
+N2kMsg.Add2ByteDouble(LongitudalSpeedWaterRef,0.001);
+N2kMsg.Add2ByteDouble(TraverseSpeedWaterRef,0.001);
+N2kMsg.Add2ByteDouble(LongitudalSpeedGroundRef,0.001);
+N2kMsg.Add2ByteDouble(TraverseSpeedGroundRef,0.001);
+N2kMsg.Add2ByteDouble(SternSpeedWaterRef,0.001);
+N2kMsg.Add2ByteDouble(SternSpeedGroundRef,0.001);
 }
 
 
-/********************************************************************************************/
-//*****************************************************************************
+
 // Set AIS UTC and Date Report
 void SetN2kPGN129793(tN2kMsg &N2kMsg, uint8_t &MessageID, tN2kAISRepeat &Repeat, uint32_t &UserID,
   double &Latitude, double &Longitude, bool &Accuracy, bool &RAIM, uint32_t &PositionTime,
